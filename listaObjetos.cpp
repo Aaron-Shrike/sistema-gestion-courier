d06ae083079b34313014e95b905330fd7fdd3f95
Aarón Rojas <86794731+Aaron-Shrike@users.noreply.github.com>
2021-08-04 00:01:25 -0500

    Update listaObjetos.cpp

--- listaObjetos.cpp
+++ listaObjetos.cpp
@@ -1,176 +1,175 @@
 // Objeto :
 
 class Objeto{
- private:
+  private:
  	int tipoObjeto;
-   float peso;
-   char descripcion[LIM];
+   	float peso;
+   	char descripcion[LIM];
 
- public:
+  public:
  	Objeto(){
-   	this->tipoObjeto = 0;
-      this->peso = 0.0;
-      this->descripcion[0]=NULL;
-   }
-
-   void leer(int &aux,int auxTipo){
-
-   	if(auxTipo == 1){
-      	objetoNacional(aux);
-      }else{
-      	objetoInternacional(aux);
-      }
-   }
-
-   void objetoNacional(int aux){
-   	char *magnitudes[2] = {"Gramos(gr.)","Kilogramos(Kg.)"};
-   	char *tipos[2] = {"Sobre","Paquete"};
-      int op;
-      int tipoObjeto;
-
-      cout << "--- OBJETO [Nro. " << (aux+1) << "] ---" << endl;
-		cout << endl;
-
-      for(int i=0;i<2;i++){
-      	cout << "   " << (i+1) << ". " << magnitudes[i] << endl;
-      }
-
-      op = leeEntero(" Opcion: ",1,2," * Opcion no valida");
-
-      cout << endl;
-      if(op == 1){
-      	this->peso = leeReal(" > Peso: ",1.0,999.0,"	  * Peso no valido");
-         this->peso = this->peso/1000;
-      }else{
-      	this->peso = leeReal(" > Peso: ",1.0,25.00,"	  * Peso no valido");
-      }
-
-      if(this->peso > 1.3){
-      	this->tipoObjeto = 2;
-      }else{
-         cout << endl;
-      	for(int i=0;i<2;i++){
-      		cout << "	" << (i+1) << ". " << tipos[i] << endl;
-      	}
-
-      	this->tipoObjeto = leeEntero(" > Tipo de Objeto: ",1,2,"	*Tipo no valida");
-   	}
-      cout << endl;
-      cout << " > Descripcion: "; cin.getline(this->descripcion,LIM-1);
-      cout << endl;
-   }
-
-   void objetoInternacional(int aux){
-   	char *magnitudes[2] = {"Gramos(gr.)","Kilogramos(Kg.)"};
-   	char *tipos[2] = {"Sobre","Paquete"};
-      int op;
-      int tipoObjeto;
-
-      cout << "--- OBJETO [Nro. " << (aux+1) << "] ---" << endl;
-		cout << endl;
-
-      for(int i=0;i<2;i++){
-      	cout << "   " << (i+1) << ". " << magnitudes[i] << endl;
-      }
-
-      op = leeEntero(" Opcion: ",1,2," * Opcion no valida");
-
-      cout << endl;
-      if(op == 1){
-      	this->peso = leeReal(" > Peso: ",1.0,999.0,"	  * Peso no valido");
-         this->peso = (float) this->peso/1000;
-      }else{
-      	this->peso = leeReal(" > Peso: ",1.0,5.00,"	  * Peso no valido");
-      }
-
-      cout << endl;
-      if(this->peso > 0.5){
-      	this->tipoObjeto = 2;
-      }else{
-      	for(int i=0;i<2;i++){
-      		cout << "	" << (i+1) << ". " << tipos[i] << endl;
-      	}
-
-      	this->tipoObjeto = leeEntero(" > Tipo de Objeto: ",1,2,"	*Tipo no valida");
+   		this->tipoObjeto = 0;
+      	this->peso = 0.0;
+      	this->descripcion[0]=NULL;
    	}
 
-      cout << endl;
-      cout << " > Descripcion: "; cin.getline(this->descripcion,LIM-1);
-      cout << endl;
-   }
+   	void leer(int &aux,int auxTipo){
+   		if(auxTipo == 1){
+      		objetoNacional(aux);
+      	}else{
+      		objetoInternacional(aux);
+      	}
+   	}
 
-   void mostrarObjeto(){
-      char *tipos[2] = {"Sobre","Paquete"};
+   	void objetoNacional(int aux){
+   		char *magnitudes[2] = {"Gramos(gr.)","Kilogramos(Kg.)"};
+   		char *tipos[2] = {"Sobre","Paquete"};
+      	int op;
+      	int tipoObjeto;
 
-   	cout << "	Tipo de objeto: " << tipos[this->tipoObjeto-1] << endl;
-      cout << "	Peso: " << this->peso << " Kg." << endl;
-      cout << "	Descripcion: " << this->descripcion << endl;
-   }
+      	cout << "--- OBJETO [Nro. " << (aux+1) << "] ---" << endl;
+		cout << endl;
 
-   int getTipoObjeto(){
-   	return this->tipoObjeto;
-   }
+      	for(int i=0;i<2;i++){
+      		cout << "   " << (i+1) << ". " << magnitudes[i] << endl;
+      	}
 
-   float getPeso(){
-   	return this->peso;
-   }
+      	op = leeEntero(" Opcion : ",1,2,"\t* Opcion no valida");
 
-   void getDescripcion(char *aux){
-   	strcpy(aux,this->descripcion);
-   }
+      	cout << endl;
+      	if(op == 1){
+      		this->peso = leeReal(" > Peso[1-999]: ",1.0,999.0,"\t* Peso no valido");
+         	this->peso = this->peso/1000;
+      	}else{
+      		this->peso = leeReal(" > Peso[1-25]: ",1.0,25.00,"\t* Peso no valido");
+      	}
 
+      	if(this->peso > 1.3){
+      		this->tipoObjeto = 2;
+      	}else{
+         	cout << endl;
+      		for(int i=0;i<2;i++){
+      			cout << "	" << (i+1) << ". " << tipos[i] << endl;
+      		}
+
+      		this->tipoObjeto = leeEntero(" > Tipo de Objeto : ",1,2,"	*Tipo no valida");
+   		}
+      	cout << endl;
+      	cin.ignore();
+      	cout << " > Descripcion : "; cin.getline(this->descripcion,LIM-1);
+      	cout << endl;
+  	}
+
+   	void objetoInternacional(int aux){
+   		char *magnitudes[2] = {"Gramos(gr.)","Kilogramos(Kg.)"};
+   		char *tipos[2] = {"Sobre","Paquete"};
+      	int op;
+      	int tipoObjeto;
+
+      	cout << "--- OBJETO [Nro. " << (aux+1) << "] ---" << endl;
+		cout << endl;
+
+      	for(int i=0;i<2;i++){
+      		cout << "   " << (i+1) << ". " << magnitudes[i] << endl;
+      	}
+
+      	op = leeEntero(" Opcion : ",1,2,"\t* Opcion no valida");
+
+      	cout << endl;
+      	if(op == 1){
+      		this->peso = leeReal(" > Peso[1-999]: ",1.0,999.0,"\t* Peso no valido");
+         	this->peso = (float) this->peso/1000;
+      	}else{
+      		this->peso = leeReal(" > Peso[1-5]: ",1.0,5.00,"\t* Peso no valido");
+      	}
+
+      	cout << endl;
+      	if(this->peso > 0.5){
+      		this->tipoObjeto = 2;
+     	}else{
+      		for(int i=0;i<2;i++){
+      			cout << "	" << (i+1) << ". " << tipos[i] << endl;
+      		}
+
+      		this->tipoObjeto = leeEntero(" > Tipo de Objeto : ",1,2,"	*Tipo no valida");
+   		}
+
+      	cout << endl;
+      	cout << " > Descripcion : "; cin.getline(this->descripcion,LIM-1);
+      	cout << endl;
+   	}
+
+   	void mostrarObjeto(){
+      	char *tipos[2] = {"Sobre","Paquete"};
+
+   		cout << "	Tipo de objeto : " << tipos[this->tipoObjeto-1] << endl;
+      	cout << "	Peso : " << this->peso << " Kg." << endl;
+      	cout << "	Descripcion : " << this->descripcion << endl;
+   	}
+
+   	int getTipoObjeto(){
+   		return this->tipoObjeto;
+   	}
+
+   	float getPeso(){
+   		return this->peso;
+   	}
+
+   	void getDescripcion(char *aux){
+   		strcpy(aux,this->descripcion);
+   	}
 };
 
 class ListaObjetos{
- private:
+  private:
  	int n;
-   int max;
-   Objeto **data;
+   	int max;
+   	Objeto **data;
 
- public:
+  public:
  	ListaObjetos(){
-   	this->n = 0;
-      this->max = 0;
-      this->data = NULL;
-   }
+   		this->n = 0;
+      	this->max = 0;
+      	this->data = NULL;
+   	}
 
-   ~ListaObjetos(){
-   	if(this->max > 0){
-      	delete []this->data;
-      }
-      this->n = 0;
-      this->max = 0;
-   }
+   	~ListaObjetos(){
+   		if(this->max > 0){
+      		delete []this->data;
+      	}
+      	this->n = 0;
+      	this->max = 0;
+   	}
 
-   int getN(){
-   	return this->n;
-   }
+   	int getN(){
+   		return this->n;
+   	}
 
-   Objeto *get(int pos){
+   	Objeto *get(int pos){
   		return this->data[pos];
-   }
+   	}
 
-   void agregar(Objeto *objeto){
-   	if(this->n == this->max){
-      	this->crecer();
-      }
-      this->data[this->n] = objeto;
-      this->n++;
-   }
+   	void agregar(Objeto *objeto){
+   		if(this->n == this->max){
+      		this->crecer();
+      	}
+      	this->data[this->n] = objeto;
+      	this->n++;
+   	}
 
- private:
+  private:
  	void crecer(){
-   	Objeto **aux;
+   		Objeto **aux;
 
-      this->max += DELTA;
-      aux = new Objeto *[this->max];
+      	this->max += DELTA;
+      	aux = new Objeto *[this->max];
 
-      for(int i=0;i<this->n;i++){
-      	aux[i] = this->data[i];
-      }
-      if(this->n >0){
-      	delete []this->data;
-      }
-      this->data = aux;
-   }
+      	for(int i=0;i<this->n;i++){
+      		aux[i] = this->data[i];
+      	}
+      	if(this->n >0){
+      		delete []this->data;
+      	}
+      	this->data = aux;
+   	}
 };
